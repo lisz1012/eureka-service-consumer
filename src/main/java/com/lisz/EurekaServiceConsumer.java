957c5b9ced15d6cda78fb6721102790ab684ae60
lisz1012 <lisz1012@163.com>
2022-03-23 00:03:23 -0700

    Remove comments

--- src/main/java/com/lisz/EurekaServiceConsumer.java
+++ src/main/java/com/lisz/EurekaServiceConsumer.java
@@ -3,10 +3,6 @@ package com.lisz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
 public class EurekaServiceConsumer {
     public static void main( String[] args ){
